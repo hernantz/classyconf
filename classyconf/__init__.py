c3cc5a5e7c459fa13add83e7fda50278bd04b307
Hernan Lozano <hernantz@gmail.com>
2020-06-06 00:00:52 -0300

    Added __version__

--- classyconf/__init__.py
+++ classyconf/__init__.py
@@ -2,3 +2,6 @@ from .casts import Boolean, Identity, List, Option, Tuple
 from .configuration import (NOT_SET, Configuration, Value, as_boolean, as_is,
                             as_list, as_option, as_tuple, evaluate)
 from .loaders import EnvFile, Environment, IniFile, env_prefix
+
+
+__version__ = "0.1.0"
