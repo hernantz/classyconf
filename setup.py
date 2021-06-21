7d088ba51adfe5ca1080dd7e02fb23c805befd41
Hernan Lozano <hernantz@gmail.com>
2021-06-20 21:13:09 -0300

    Moved deps to dev

--- setup.py
+++ setup.py
@@ -1,24 +1,5 @@
-from setuptools import setup, Command
-
-version = "0.4.0"
-
-class VersionCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        print(version)
+import setuptools
 
 
 if __name__ == "__main__":
-    setup(
-        version=version,
-        packages=["classyconf"],
-        download_url="https://github.com/hernantz/classyconf/tarball/{}".format(version),
-        cmdclass={"version": VersionCommand},
-    )
+    setuptools.setup()
